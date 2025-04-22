0ef572b43044fe446c8dbb064cfb6209a15974eb
RAM <153575188+Rxm46@users.noreply.github.com>
2025-04-22 22:12:58 +0530

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+console.log("API URL:", import.meta.env.VITE_API_URL);
 createRoot(document.getElementById("root")!).render(<App />);
